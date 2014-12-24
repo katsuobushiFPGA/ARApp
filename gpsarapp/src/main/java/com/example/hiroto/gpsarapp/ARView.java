30757e94a1ecee4e62a1d19cb41df813cb5f4a8a
katsuobushiFPGA <suizmelon@gmail.com>
2014-12-24 22:04:31 +0900

    first commit

---
+++ gpsarapp/src/main/java/com/example/hiroto/gpsarapp/ARView.java
@@ -0,0 +1,201 @@
+package com.example.hiroto.gpsarapp;
+
+/**
+ * Created by hiroto on 2014/12/24.
+ */
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.maps.GeoPoint;
+
+import java.util.ArrayList;
+
+//public class ARView extends SurfaceView implements SurfaceHolder.Callback {
+public class ARView extends View {
+
+    // カメラの画角を指定する
+    private final int ANGLE = 60;
+    // ARテキストの見える範囲を指定する
+    // ここでは10kmほどに指定する
+    private final float VIEW_LIMIT = 100000;
+
+    // コンパスの描画位置を指定する
+    private final float POS_COMPASSX = 20;
+    private final float POS_COMPASSY = 20;
+
+    // 向きを保持する変数
+    float direction;
+
+    // 現在地を保持する変数
+    int posx, posy;
+
+    // ARテキストの情報を保持するオブジェクト
+    private ArrayList<GPSData> list;
+
+    // ディスプレイサイズ
+    private int displayX;
+
+    public ARView(Context context, Cursor cursor) {
+        super(context);
+
+        // データベースの読み込みを行う
+        readTable(cursor);
+
+        // 画面サイズの取得
+        Display disp = ((WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        displayX = disp.getWidth();
+    }
+
+    public void readTable(Cursor cursor) {
+        // データベースに保存されている
+        // 全てのARテキストの情報をlistに読み込む
+        if (list != null)
+            list.clear();
+        list = new ArrayList<GPSData>();
+
+        cursor.moveToFirst();
+        do {
+            GPSData data = new GPSData();
+            data.info = cursor.getString(0);
+            data.latitude = cursor.getInt(1);
+            data.longitude = cursor.getInt(2);
+            list.add(data);
+        } while (cursor.moveToNext());
+    }
+
+    class GPSData {
+        public String info;
+        public int latitude;
+        public int longitude;
+    }
+
+    // 描画処理
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+
+        for (int i = 0; i < list.size(); i++) {
+            // データの読み込み
+            GPSData data = list.get(i);
+            String info = data.info;
+            int y = data.latitude;
+            int x = data.longitude;
+
+            // ARテキストとの距離を求める
+            double dx = (x - posx);
+            double dy = (y - posy);
+            float distance = (float) Math.sqrt(Math.pow(dy, 2)
+                    + Math.pow(dx, 2));
+
+            // ARテキストとの距離が一定以上離れていたら、処理を行わずに次のARテキストの処理を行う
+            if (distance > VIEW_LIMIT) {
+                continue;
+            }
+
+            // ARテキストと現在地のなす角を求めて正規化する
+            double angle = Math.atan2(dy, dx);
+            float degree = (float) Math.toDegrees(angle);
+            degree = -degree + 90;
+            if (degree < 0)
+                degree = 360 + degree;
+
+            // 端末の向きとARテキストとの角度の差を求める
+            float sub = degree - direction;
+            if (sub < -180.0)
+                sub += 360;
+            if (sub > 180.0)
+                sub -= 360;
+
+            // ARテキストが視野に存在すれば描画処理を行う
+            if (Math.abs(sub) < (ANGLE / 2)) {
+                // 距離によってARテキストのサイズを決める
+                float textSize = 50 * (float) (VIEW_LIMIT - distance)
+                        / VIEW_LIMIT;
+                paint.setTextSize(textSize);
+
+                // ARテキストの描画を描画する
+                float textWidth = paint.measureText(info);
+                float diff = (sub / (ANGLE / 2)) / 2;
+                float left = (displayX / 2 + displayX * diff) - (textWidth / 2);
+                drawBalloonText(canvas, paint, info, left, 55);
+            }
+        }
+
+        // コンパスを描画する
+        drawCompass(canvas, paint);
+    }
+
+    private void drawBalloonText(Canvas canvas, Paint paint, String text,
+                                 float left, float top) {
+        // 文字列の幅を取得
+        float textWidth = paint.measureText(text);
+        // フォント情報の取得
+        FontMetrics fontMetrics = paint.getFontMetrics();
+
+        // 文字列の5ポイント外側を囲む座標を求める
+        float bLeft = left - 5;
+        float bRight = left + textWidth + 5;
+        float bTop = top + fontMetrics.ascent - 5;
+        float bBottom = top + fontMetrics.descent + 5;
+
+        // 吹き出しの描画
+        RectF rectF = new RectF(bLeft, bTop, bRight, bBottom);
+        paint.setColor(Color.LTGRAY);
+        canvas.drawRoundRect(rectF, 5, 5, paint);
+
+        // 三角形の描画
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        Path path = new Path();
+        float center = left + textWidth / 2;
+        float triangleSize = paint.getTextSize() / 3;
+        path.moveTo(center, bBottom + triangleSize);
+        path.lineTo(center - triangleSize / 2, bBottom - 1);
+        path.lineTo(center + triangleSize / 2, bBottom - 1);
+        path.lineTo(center, bBottom + triangleSize);
+        canvas.drawPath(path, paint);
+
+        // 文字列の描画
+        paint.setColor(Color.WHITE);
+        canvas.drawText(text, left, top, paint);
+    }
+
+    // コンパスの描画
+    private void drawCompass(Canvas canvas, Paint paint) {
+        Path path = new Path();
+        path.moveTo(POS_COMPASSX, POS_COMPASSY - 20);
+        path.lineTo(POS_COMPASSX + 10, POS_COMPASSY + 10);
+        path.lineTo(POS_COMPASSX - 10, POS_COMPASSY + 10);
+        path.moveTo(POS_COMPASSX, POS_COMPASSY - 20);
+        paint.setColor(Color.RED);
+        canvas.rotate(-direction, POS_COMPASSX, POS_COMPASSY);
+        canvas.drawPath(path, paint);
+        canvas.rotate(direction, POS_COMPASSX, POS_COMPASSY);
+        paint.setTextSize(12);
+        paint.setColor(Color.WHITE);
+        String str = new String();
+        canvas.drawText(str, 5, POS_COMPASSY * 3, paint);
+    }
+
+    public void drawScreen(float preDirection, GeoPoint geoPoint) {
+        // センサーの値から端末の向きを計算する
+        direction = (preDirection + 450) % 360;
+        // 座標情報の取得
+        if (geoPoint != null) {
+            posy = geoPoint.getLatitudeE6();
+            posx = geoPoint.getLongitudeE6();
+        }
+        // onDrawを呼び出して再描画
+        invalidate();
+    }
+}
---
+++ gpsarapp/src/main/java/com/example/hiroto/gpsarapp/GPSARApp.java
@@ -0,0 +1,281 @@
+package com.example.hiroto.gpsarapp;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.hardware.GeomagneticField;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.WindowManager.LayoutParams;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+
+import java.util.Date;
+import java.util.List;
+
+public class GPSARApp extends Activity implements SensorEventListener,
+        LocationListener, View.OnClickListener {
+
+    private SensorManager sensorManager;
+    private float[] accelerometerValues = new float[3];
+    private float[] magneticValues = new float[3];
+    List<Sensor> listMag;
+    List<Sensor> listAcc;
+
+    private ARView arView;
+
+    private LocationManager locationManager;
+    private GeoPoint geoPoint;
+    private GeomagneticField geomagneticField;
+
+    // データベースで使用する変数
+    private final static String DB_NAME = "gps_data.db";
+    private final static String DB_TABLE = "gps_data";
+    private final static int DB_VERSION = 1;
+    private SQLiteDatabase db;
+    Cursor cursor;
+
+    private static final int BUTTON_SIZE = 80;
+    EditText editText;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // データベースの用意
+        initData();
+
+        // フルスクリーン指定
+        getWindow().clearFlags(
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // ARViewの取得
+        arView = new ARView(this, cursor);
+        cursor.close();
+
+        // 各種センサーの用意
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        listMag = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD);
+        listAcc = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+
+        // 画面サイズの取得
+        Display disp = ((WindowManager) this
+                .getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int displayX = disp.getWidth();
+
+        // 登録処理を行うlayoutの作成
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.HORIZONTAL);
+        layout.setGravity(Gravity.BOTTOM | Gravity.RIGHT);
+        // 登録文字
+        editText = new EditText(this);
+        editText.setWidth(displayX - BUTTON_SIZE);
+        // 登録ボタン
+        Button button = new Button(this);
+        button.setText("Register");
+        button.setOnClickListener(this);
+        button.setWidth(BUTTON_SIZE);
+        // 各要素の登録
+        layout.addView(editText, 0);
+        layout.addView(button, 1);
+
+        // Viewの重ね合わせ
+        setContentView(new CameraView(this));
+        addContentView(arView, new LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
+        addContentView(layout, new LayoutParams(LayoutParams.FILL_PARENT,
+                LayoutParams.FILL_PARENT));
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // ロケーションマネージャの設定
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                0, this);
+        // センサー処理の登録
+        sensorManager.registerListener(this, listMag.get(0),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, listAcc.get(0),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(this);
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    // センサー値の反映
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        switch (event.sensor.getType()) {
+            case Sensor.TYPE_ACCELEROMETER:
+                accelerometerValues = event.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                magneticValues = event.values.clone();
+                break;
+        }
+
+        if (magneticValues != null && accelerometerValues != null
+                && geomagneticField != null) {
+            float[] R = new float[16];
+            float[] I = new float[16];
+
+            SensorManager.getRotationMatrix(R, I, accelerometerValues,
+                    magneticValues);
+
+            float[] actual_orientation = new float[3];
+
+            SensorManager.getOrientation(R, actual_orientation);
+
+            // 求まった方位角をラジアンから度に変換する
+            float direction = (float) Math.toDegrees(actual_orientation[0])
+                    + geomagneticField.getDeclination();
+            arView.drawScreen(direction, geoPoint);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location arg0) {
+        geoPoint = new GeoPoint((int) (arg0.getLatitude() * 1E6), (int) (arg0
+                .getLongitude() * 1E6));
+        geomagneticField = new GeomagneticField((float) arg0.getLatitude(),
+                (float) arg0.getLongitude(), (float) arg0.getAltitude(),
+                new Date().getTime());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    public void initData() {
+        // SQLiteOpenHelperを継承したクラスを使用してデータベースを作成します
+        SQLiteOpenHelperEx helper = new SQLiteOpenHelperEx(this);
+        db = helper.getWritableDatabase();
+
+        cursor = db.query(DB_TABLE, new String[] { "info", "latitude",
+                "longitude" }, null, null, null, null, null);
+        // テーブルが空の時内容をセットする
+        if (cursor.getCount() < 1) {
+            presetTable();
+            cursor = db.query(DB_TABLE, new String[] { "info", "latitude",
+                    "longitude" }, null, null, null, null, null);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (editText.getText().toString().equals("")) {
+            Toast.makeText(this, "テキストを入力してください", Toast.LENGTH_LONG).show();
+        } else if (geoPoint == null) {
+            Toast.makeText(this, "位置情報が取得できません", Toast.LENGTH_LONG).show();
+        } else {
+            ContentValues values = new ContentValues();
+            values.put("info", editText.getText().toString());
+            values.put("latitude", geoPoint.getLatitudeE6());
+            values.put("longitude", geoPoint.getLongitudeE6());
+            db.insert(DB_TABLE, "", values);
+            cursor = db.query(DB_TABLE, new String[] { "info", "latitude",
+                    "longitude" }, null, null, null, null, null);
+            arView.readTable(cursor);
+            cursor.close();
+            editText.setText("");
+            Toast.makeText(this, "テキストが登録されました", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void presetTable() {
+        // テーブルの内容が空の時以下の内容をセットする
+        ContentValues values = new ContentValues();
+        values.put("info", "安田講堂");
+        values.put("latitude", 35713433);
+        values.put("longitude", 139762594);
+        db.insert(DB_TABLE, "", values);
+        values.put("info", "東京ドーム");
+        values.put("latitude", 35705593);
+        values.put("longitude", 139752252);
+        db.insert(DB_TABLE, "", values);
+        values.put("info", "東京スカイツリー");
+        values.put("latitude", 35710084);
+        values.put("longitude", 139810751);
+        db.insert(DB_TABLE, "", values);
+        values.put("info", "明治神宮");
+        values.put("latitude", 35676402);
+        values.put("longitude", 139700174);
+        db.insert(DB_TABLE, "", values);
+        values.put("info", "国会議事堂");
+        values.put("latitude", 35675844);
+        values.put("longitude", 139745578);
+        db.insert(DB_TABLE, "", values);
+        values.put("info", "谷中銀座");
+        values.put("latitude", 35727594);
+        values.put("longitude", 139765215);
+        db.insert(DB_TABLE, "", values);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    public class SQLiteOpenHelperEx extends SQLiteOpenHelper {
+//      コンストラクタ
+        public SQLiteOpenHelperEx(Context context) {
+            super(context, DB_NAME, null, DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // テーブルの作成
+            String sql = "create table if not exists " + DB_TABLE
+                    + "(info text, latitude numeric, longitude numeric)";
+            db.execSQL(sql);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // データベースのアップグレード
+            // ここでは、テーブルを作り直しをしています
+            db.execSQL("drop table if exists " + DB_TABLE);
+            onCreate(db);
+        }
+    }
+
+}
