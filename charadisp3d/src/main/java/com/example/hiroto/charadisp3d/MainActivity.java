85e124476164fd4e705f6dad6aab670bb95fb07f
katsuobushiFPGA <suizmelon@gmail.com>
2015-01-05 23:06:00 +0900

    OpenGLES Sheder Sample add.

--- charadisp3d/src/main/java/com/example/hiroto/charadisp3d/MainActivity.java
+++ charadisp3d/src/main/java/com/example/hiroto/charadisp3d/MainActivity.java
@@ -1,17 +1,45 @@
 package com.example.hiroto.charadisp3d;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    private GLSurfaceView mGLSurfaceView;
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        GLSurfaceView glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setRenderer(new OpenGLSample());
-        setContentView(glSurfaceView);
+        mGLSurfaceView = new GLSurfaceView(this);
+
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        // 端末がOpenGL ES 2.0をサポートしているかチェック
+        if (configurationInfo.reqGlEsVersion >= 0x20000) {
+            mGLSurfaceView.setEGLContextClientVersion(2);  // OpenGLバージョンを設定
+            mGLSurfaceView.setRenderer(new MyRenderer());  // レンダラを設定
+        } else {
+            return;
+        }
+
+        setContentView(mGLSurfaceView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();  // 忘れずに！
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();  // 忘れずに！
     }
 }
