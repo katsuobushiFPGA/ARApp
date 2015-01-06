fbd2a5b4e3b6219fec12897875fcbc7ae1e87981
katsuobushiFPGA <suizmelon@gmail.com>
2015-01-06 23:37:10 +0900

    glClearColor changed.(Droid Renderer)

--- gl20modelview/src/main/java/com/example/hiroto/gl20modelview/MainActivity.java
+++ gl20modelview/src/main/java/com/example/hiroto/gl20modelview/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 
 //3Dモデルの読み込み
 public class MainActivity extends Activity {
@@ -14,14 +16,18 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-
         //GLサーフェイスビュー
         glView=new GLSurfaceView(this);
         glView.setEGLContextClientVersion(2);//OpenGL ES 2.0を使用
         glView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         glView.setRenderer(new GLRenderer2(this));
-        glView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-        setContentView(glView);
+        glView.getHolder().setFormat(PixelFormat.RGBA_8888);
+        glView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_GPU);
+//        setContentView(glView);
+        FrameLayout layout = new FrameLayout(this);
+        setContentView(layout);
+        addContentView(glView, new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.FILL_PARENT));
         addContentView(new CameraView(this), new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.FILL_PARENT, WindowManager.LayoutParams.FILL_PARENT));
     }
